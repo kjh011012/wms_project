33ec1b4b6b2763c35de93b7541208643e76e524f
kjh011012 <136458313+kjh011012@users.noreply.github.com>
2025-05-29 20:02:23 +0900

    Update new_min_many_request_sp_5030.py

--- manager/backend/spio/new_min_many_request_sp_5030.py
+++ manager/backend/spio/new_min_many_request_sp_5030.py
@@ -53,10 +53,10 @@ except Exception as e:
     model = None
 
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "wms",
-    "password": "1234",
-    "database": "backend",
+    "host": "연결호스트",
+    "user": "사용자이름",
+    "password": "비밀번호",
+    "database": "데이터베이스이름"
 }
 
 
